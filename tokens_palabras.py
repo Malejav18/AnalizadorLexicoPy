9c6a70fd0015f6becc38cebd1a2ed57755e27e16
Alevv <134009912+Malejav18@users.noreply.github.com>
2025-03-14 10:04:18 -0500

    Update tokens_palabras.py

--- tokens_palabras.py
+++ tokens_palabras.py
@@ -1,79 +1,65 @@
 # Diccionario de librerías y sus palabras reservadas
 palabras_reservadas_librerias = {
     # Módulo math
-    'math': {'pi', 'e', 'tau', 'inf', 'nan', 'sqrt', 'ceil', 'floor', 'trunc', 'pow', 'exp', 'log', 
-             'log10', 'log2', 'sin', 'cos', 'tan', 'asin', 'acos', 'atan', 'atan2', 'degrees', 
-             'radians', 'hypot', 'factorial', 'gcd', 'lcm', 'isfinite', 'isinf', 'isnan'},
+    'math',
 
     # Módulo os
-    'os': {'path', 'getenv', 'chdir', 'getcwd', 'listdir', 'mkdir', 'remove', 'rename', 'rmdir', 
-           'system', 'environ'},
+    'os',
 
     # Módulo sys
-    'sys': {'exit', 'argv', 'path', 'stdin', 'stdout', 'stderr', 'platform'},
+    'sys',
 
     # Módulo datetime
-    'datetime': {'datetime', 'date', 'time', 'timedelta', 'timezone', 'now', 'today', 'utcnow', 
-                 'strftime', 'strptime'},
+    'datetime',
 
     # Módulo random
-    'random': {'randint', 'uniform', 'choice', 'choices', 'sample', 'shuffle', 'seed'},
+    'random',
 
     # Módulo re (Expresiones Regulares)
-    're': {'match', 'search', 'findall', 'sub', 'split', 'compile'},
+    're',
 
     # Librería numpy
-    'numpy': {'array', 'linspace', 'arange', 'zeros', 'ones', 'eye', 'dot', 'matmul', 'mean', 'median', 
-              'std', 'var', 'min', 'max', 'sum', 'prod', 'shape', 'dtype', 'reshape', 'transpose', 
-              'flatten', 'squeeze'},
+    'numpy',
 
     # Librería pandas
-    'pandas': {'DataFrame', 'Series', 'read_csv', 'read_excel', 'to_csv', 'to_excel', 'loc', 'iloc', 
-               'head', 'tail', 'merge', 'concat', 'groupby', 'pivot', 'apply', 'map', 'unique', 
-               'value_counts', 'fillna', 'dropna', 'drop', 'rename'},
+    'pandas',
 
     # Librería matplotlib.pyplot
-    'matplotlib.pyplot': {'plot', 'scatter', 'bar', 'hist', 'boxplot', 'xlabel', 'ylabel', 'title', 
-                          'legend', 'grid', 'show', 'savefig'},
+    'matplotlib',
 
     # Librería sklearn
-    'sklearn': {'fit', 'predict', 'transform', 'score', 'train_test_split', 'cross_val_score', 
-                'accuracy_score', 'confusion_matrix', 'classification_report'},
+    'sklearn',
 
     # Flask
-    'flask': {'Flask', 'request', 'jsonify', 'render_template', 'session', 'redirect', 'url_for'},
+    'flask',
 
     # Django
-    'django': {'models', 'CharField', 'IntegerField', 'ForeignKey', 'ManyToManyField', 'views', 
-               'forms', 'admin', 'urls', 'QuerySet', 'filter', 'get', 'all', 'save', 'delete'},
+    'django',
 
     # FastAPI
-    'fastapi': {'FastAPI', 'APIRouter', 'HTTPException', 'Request', 'Response', 'Depends'},
+    'fastapi',
 
     # SQL (usado en sqlite3, MySQL, etc.)
-    'sql': {'SELECT', 'FROM', 'WHERE', 'JOIN', 'INNER', 'LEFT', 'RIGHT', 'FULL', 'GROUP', 'ORDER', 
-            'LIMIT', 'OFFSET', 'INSERT', 'INTO', 'VALUES', 'UPDATE', 'SET', 'DELETE', 'CREATE', 
-            'TABLE', 'DROP', 'ALTER', 'PRIMARY', 'FOREIGN', 'KEY', 'AUTO_INCREMENT', 'INDEX'},
+    'sql',
 
     # MongoDB (pymongo)
-    'pymongo': {'find', 'insert_one', 'insert_many', 'update_one', 'update_many', 'delete_one', 
-                'delete_many', 'aggregate', 'sort', 'limit'},
+    'pymongo',
 
     # asyncio
-    'asyncio': {'async', 'await', 'create_task', 'run', 'sleep', 'gather', 'Event', 'Queue'},
+    'asyncio',
 
     # Manejo de archivos (open)
-    'file': {'read', 'readline', 'readlines', 'write', 'writelines', 'close', 'seek', 'tell', 'flush'},
+    'file',
 
     # JSON
-    'json': {'load', 'loads', 'dump', 'dumps'},
+    'json' ,
 
     # XML
-    'xml': {'parse', 'fromstring', 'find', 'findall', 'get', 'set', 'write'},
+    'xml',
 
     # Requests (para peticiones HTTP)
-    'requests': {'get', 'post', 'put', 'delete', 'json', 'headers', 'status_code', 'text'},
+    'requests',
 
     # Sockets
-    'socket': {'socket', 'bind', 'listen', 'accept', 'connect', 'send', 'recv', 'close'},
+    'socket'
 }
